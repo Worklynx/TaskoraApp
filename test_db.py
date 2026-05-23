20c3c7989bd7cff4ec4da05cd2f8b3eb8cdcb1ed
Obumneme-1 <obumneme.madubuko@pau.edu.ng>
2026-05-23 18:45:33 +0100

    adding the test_db file

---
+++ test_db.py
@@ -0,0 +1,8 @@
+from database import get_connection
+
+try:
+    conn = get_connection()
+    print("✅ Connected to PostgreSQL successfully!")
+    conn.close()
+except Exception as e:
+    print("❌ Connection failed:", e)
